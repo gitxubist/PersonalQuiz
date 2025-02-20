d53ce21d427372a0c477d7ee94e34de69373670b
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-20 23:19:05 +0300

    test

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -13,8 +13,8 @@ final class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
         print(transferAnswers)
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
